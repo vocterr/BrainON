740bd1ea02fe67ac053f57698d80908a8b41dd22
vocterr <kozanecki.bartosz@interia.pl>
2025-06-20 00:05:37 +0200

    123

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,6 @@
 // app/api/auth/[...nextauth]/route.ts
 
 import NextAuth, { AuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
-import prisma from "@/prisma/prisma";
 import { authOptions } from "@/lib/auth";
 
 
--- app/api/calls/route.ts
+++ app/api/calls/route.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/prisma/prisma';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../login/route';
+import { authOptions } from '@/lib/auth';
 
 export async function POST(request: Request) {
     const session = await getServerSession(authOptions);
