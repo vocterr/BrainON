e0a1e994b9505d22941dbe91f3c360226b47fc18
vocterr <kozanecki.bartosz@interia.pl>
2025-06-29 17:27:14 +0200

    xd

--- app/api/ice-servers/route.ts
+++ app/api/ice-servers/route.ts
@@ -17,22 +17,30 @@ export async function GET() {
     }
 
     const iceServers = [
-        {
-            urls: "turn:turn.metered.ca:80",
-            username,
-            credential,
-        },
-        {
-            urls: "turn:turn.metered.ca:443",
-            username,
-            credential,
-        },
-        {
-            urls: "turn:turn.metered.ca:443?transport=tcp",
-            username,
-            credential,
-        }
-    ];
+      {
+        urls: "stun:stun.relay.metered.ca:80",
+      },
+      {
+        urls: "turn:global.relay.metered.ca:80",
+        username,
+        credential
+      },
+      {
+        urls: "turn:global.relay.metered.ca:80?transport=tcp",
+        username,
+        credential
+      },
+      {
+        urls: "turn:global.relay.metered.ca:443",
+        username,
+        credential
+      },
+      {
+        urls: "turns:global.relay.metered.ca:443?transport=tcp",
+        username,
+        credential
+      },
+  ]
 
     return NextResponse.json({ iceServers });
 }
